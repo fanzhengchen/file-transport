900589e7798a7284ae0a3f14eeca738a4a1ba8fd
shengxun <shengxun@qunhe.com>
2020-08-03 21:07:14 +0800

    grpc server command line

--- server/server.go
+++ server/server.go
@@ -1,18 +1,30 @@
 package main
 
-
 import (
 	"file-transport/proto"
 	"file-transport/transport"
+	"flag"
+	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"net"
 )
 
+var (
+	h bool
+)
 
 
 func main() {
-	listener, err := net.Listen("tcp", ":4040")
+
+	port := flag.Int("port", 4040, "grpc server port")
+	network := flag.String("network", "tcp", "network type")
+
+
+	flag.Parse()
+
+	address := fmt.Sprintf(":%d", port)
+	listener, err := net.Listen(*network, address)
 	if err != nil {
 		panic(err)
 	}
